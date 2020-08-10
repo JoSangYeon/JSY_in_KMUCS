5265c45b14d9ea9a1a76d8a63927917859a79b49
SangYeonJO <whtkddus98@kookmin.ac.kr>
2020-08-10 18:06:22 +0900

    인접행렬과 인접리스트를 통한 Graph
    인접 행렬(G1  class)과 인접 리스트(G2 class)의 형태로 Graph를 구현하고 DFS&BFS 알고리즘을 각각 구현해 보았습니다.

--- Graph/Main.java
+++ Graph/Main.java
@@ -28,15 +28,37 @@ public class Main
 				{0, 0, 0, 1, 1, 1, 1, 0}
 				};
 		
+		String data = 
+				"0 1 2\n" + 
+				"1 0 3 4\n" + 
+				"2 0 5 6\n" + 
+				"3 1 7\n" + 
+				"4 1 7\n" + 
+				"5 2 7\n" + 
+				"6 2 7\n" + 
+				"7 3 4 5 6\n";
+		
 		G1 g1 = new G1(matrix1);
 		G1 g2 = new G1(matrix2);
 		
 		g1.print();
 		g1.BFS(0);
 		g1.DFS(0);
-		
+		System.out.println();
 		g2.print();
 		g2.BFS(0);
 		g2.DFS(0);
+		System.out.println();
+		
+		G2 g3 = new G2(matrix1);
+		G2 g4 = new G2(data);
+		
+		g3.print();
+		g3.BFS(0);
+		g3.DFS(0);
+		System.out.println();
+		g4.print();
+		g4.BFS(0);
+		g4.DFS(0);
 	}
 }
