cb1d2c091b36f68e919a0b0f8b8d7696f1e520fa
JoSangYeon <whtkddus98@kookmin.ac.kr>
2021-12-09 04:07:52 +0900

    BFS, DFS 구현(python)

--- 11.02 Graph.py
+++ 11.02 Graph.py
@@ -12,6 +12,7 @@
 3. Euler Tour 공부?->이건 뭐임??ㅋㅋㅋ
 """
 """BFS 예제 Graph의 인접행렬"""
+node_name = ['r', 's', 't', 'u', 'v', 'w', 'x', 'y']
 matrix1 = [[0, 1, 0, 0, 1, 0, 0, 0], #r
            [1, 0, 0, 0, 0, 1, 0, 0], #s
            [0, 0, 0, 1, 0, 1, 1, 0], #t
@@ -20,6 +21,16 @@ matrix1 = [[0, 1, 0, 0, 1, 0, 0, 0], #r
            [0, 1, 1, 0, 0, 0, 1, 0], #w
            [0, 0, 1, 1, 0, 1, 0, 1], #x
            [0, 0, 0, 1, 0, 0, 1, 0]] #y
+list1 = {
+    0:set([1,4]),
+    1:set([0,5]),
+    2:set([3,5,6]),
+    3:set([2,6,7]),
+    4:set([0]),
+    5:set([1,2,6]),
+    6:set([2,3,5]),
+    7:set([3,6])
+}
 
 """DFS 예제 Graph의 인접행렬1"""
 matrix2 = [[0, 1, 0, 1, 0, 0], #u
@@ -28,6 +39,15 @@ matrix2 = [[0, 1, 0, 1, 0, 0], #u
            [0, 1, 0, 0, 0, 0], #x
            [0, 0, 0, 1, 0, 0], #y
            [0, 0, 0, 0, 0, 1]] #z
+list2 = {
+    0:set([1,3]),
+    1:set([4]),
+    2:set([4,5]),
+    3:set([1]),
+    4:set([3]),
+    5:set([5]),
+}
+
 """DFS 예제 Graph의 인접행렬2"""
 matrix3 = [[0, 0, 0, 0, 1, 0, 0, 1], #s
            [0, 0, 1, 1, 0, 0, 0, 0], #t
@@ -37,3 +57,45 @@ matrix3 = [[0, 0, 0, 0, 1, 0, 0, 1], #s
            [0, 0, 0, 0, 0, 0, 0, 1], #x
            [0, 0, 0, 0, 0, 1, 0, 0], #y
            [0, 0, 0, 0, 1, 0, 1, 0]] #z
+list3 = {
+    0:set([4,7]),
+    1:set([2,3]),
+    2:set([1,3]),
+    3:set([0,4]),
+    4:set([5]),
+    5:set([7]),
+    6:set([5]),
+    7:set([4,6])
+}
+
+from collections import deque
+
+def BFS(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        u = queue.popleft()
+        if u not in visited:
+            visited.append(u)
+            queue += graph[u] - set(visited) #방문했던 node를 제외하고 인접한 node를 queue 대입
+    return visited
+
+def DFS(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        u = stack.pop()
+        if u not in visited:
+            visited.append(u)
+            stack += graph[u] - set(visited) #방문했던 node를 제외하고 인접한 node를 queue 대입
+    return visited
+
+if __name__ == "__main__":
+    # result = BFS(list1, 1)
+    result = DFS(list3, 0)
+
+    for node in result:
+        print(node_name[node], end=" ")
+    print()
